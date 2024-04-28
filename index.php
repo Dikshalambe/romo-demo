3a4ed8f899a3e42efa55150adb7dfff2f5f9be62
dikshalambe12 <lambediksha417gmail.com>
2024-04-28 17:15:55 +0530

    add new paragraph

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!-- commands -->
 <!-- git status -->
 <!-- git add <file name> or git add . -->
-<!-- git  -->
+<!-- git commit -m "some message" -->
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique quam perferendis dolorum amet, aliquid ipsum doloribus officiis expedita atque id. Consequatur ducimus minus facilis repudiandae, quae animi quas deleniti a.</p>
